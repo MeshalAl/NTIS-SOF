38db74003a663e87d2a475a19eb46ecac76f77ee
MeshalAl <meshal@alanazi.io>
2024-09-08 22:22:26 +0300

    main entrypoint -WIP

---
+++ src/main.py
@@ -0,0 +1,44 @@
+import click
+from config.config_loader import load_config 
+from pathlib import Path
+from pydantic import ValidationError
+
+@click.group()
+@click.option("-cf", "--config-path",
+              type=click.Path(exists=True),
+              show_default=True,
+              default=Path(__file__).parent / "config/defaults.yaml",
+              help="Absolute path to the config file.", 
+              required=False)
+@click.pass_context
+def start_cli(ctx, config_path: str):
+    
+    try:
+        config = load_config(config_path)
+    except ValidationError as e:
+       raise click.FileError(f"Error loading config: {e.errors().messages}", config_path
+       
+    ctx.obj.config = config
+    
+    # flow steps: cli runs the icli, fetches users, displays the results and holds. displays current page
+    # page_size, remaining quota etc.
+    # give the user options to fetch next, or previous, save to file, "view" the file, and bookmark a user.
+
+# importing so its hooked up as a registered command... 
+from cli import commands
+start_cli.add_command(commands.fetch_users)
+
+if __name__ == "__main__":
+    
+    try: 
+        start_cli()
+    except Exception as e:
+        click.echo(f"{e}", err=True, color=True, nl=True)
+
+    # the flow branches into two main paths, one for regular command and argument based cli,
+    # and the other for interactive
+    # ex command based: python main.py -{function} -{args}, so fetch_users --page-size 10 --page 1
+    # same goes on for bookmarking, ex python main.py bookmarks --add-user 293812 --add-user 293812,
+    # adding users should also trigger writing to file? cache? db? all?? not sure yet.
+    # python main.py bookmarks --remove-user 293812, maybe we can supply a list of ids to remove? would be nice
+    # python main.py bookmarks --view-bookmarks, feels very straight forward, display the bookmarks. and thats it.
