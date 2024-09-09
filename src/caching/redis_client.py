e77be44c4269e1485d8f55cb97c037a9dd60eccf
MeshalAl <meshal@alanazi.io>
2024-09-09 17:43:28 +0300

    fixed getting and setting cache. added cache age.

--- src/caching/redis_client.py
+++ src/caching/redis_client.py
@@ -1,7 +1,7 @@
 from redis import Redis
 from os import getenv
 import json
-
+from datetime import datetime
 
 class RedisClient:
 
@@ -29,28 +29,52 @@ class RedisClient:
     def set_api_cache(self, url: str, payload: dict) -> bool:
         # TODO: remove hard coded expiry
         url_key = self._api_url_string(url)
-        self.api_cache.hset(url_key, mapping=payload)
+        
+        payload.update({"cache_timestamp": datetime.now().isoformat()})
+        serialized_payload = json.dumps(payload)
+        self.api_cache.set(url_key, serialized_payload)
         self.api_cache.expire(url_key, self.default_expiry)
         return True
 
     def get_api_cache(self, url: str) -> dict | None:
         url_key = self._api_url_string(url)
-        payload = self.api_cache.get(url_key)
-        if payload:
-            payload_dict = json.loads(payload).update({"cached": True})  # type: ignore
-            return payload_dict
+        serialized_payload = self.api_cache.get(url_key)
+        if serialized_payload:
+            payload: dict = json.loads(serialized_payload)
+            cache_timestamp = payload['cache_timestamp']
+            age = self._get_cache_age(cache_timestamp)
+            payload.update({"cached": True, "cache_age": age})
+            payload.pop('cache_timestamp')
+            return payload
         return None
 
-    def set_user_cache(self, user_id: int, payload: dict) -> bool:
-        id_key = self._user_id_string(user_id)
-        self.user_cache.hset(id_key, mapping=payload)
-        self.user_cache.expire(id_key, self.default_expiry)
-        return True
+    # def set_user_cache(self, user_id: int, payload: dict) -> bool:
+    #     id_key = self._user_id_string(user_id)
+    #     self.user_cache.hset(id_key, mapping=payload)
+    #     self.user_cache.expire(id_key, self.default_expiry)
+    #     return True
 
-    def get_user_cache(self, user_id: int) -> dict | None:
-        id_key = self._user_id_string(user_id)
-        payload = self.user_cache.get(id_key)
-        if payload:
-            payload_dict = json.loads(payload).update({"cached": True})  # type: ignore
-            return payload_dict
-        return None
+    # def get_user_cache(self, user_id: int) -> dict | None:
+    #     id_key = self._user_id_string(user_id)
+    #     payload = self.user_cache.get(id_key)
+    #     if payload:
+    #         payload_dict = json.loads(payload).update({"cached": True})  # type: ignore
+    #         return payload_dict
+    #     return None
+    @staticmethod
+    def _get_cache_age(cache_timestamp: str) -> str:
+        cache_time = datetime.fromisoformat(cache_timestamp)
+        age = datetime.now() - cache_time
+        
+        if age.days > 0:
+            return f"{age.days}d"
+        
+        minutes, seconds = divmod(age.seconds, 60)
+        hours, minutes = divmod(minutes, 60)
+
+        if hours > 0:
+            return f"{hours}h {minutes}m"
+        elif minutes > 0:
+            return f"{minutes}m {seconds}s"
+        else:
+            return f"{seconds}s"
