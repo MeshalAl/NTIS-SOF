359e0d4a54620c95094afa7b4e3fbadfcf4a84d9
MeshalAl <meshal@alanazi.io>
2024-09-12 01:30:20 +0300

    resolved context issue

--- src/db/database.py
+++ src/db/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm.session import Session 
 from db.utility import load_dotenv
-
+from contextlib import contextmanager
 
 load_dotenv()
 
@@ -10,14 +10,14 @@ database_url = os.environ.get('DATABASE_URL')
 
 engine = create_engine(database_url)
 
+@contextmanager
 def get_session():
-
-    with Session(engine) as session:
-        try:
-            yield session
-            session.commit()
-        except:
-            session.rollback()
-            raise
-        finally:
-            session.close()
+    session = Session(engine)
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
