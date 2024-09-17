4b78f150b035fa23918a8ef497eb2a7ba194cc41
MeshalAl <meshal@alanazi.io>
2024-09-17 16:36:39 +0300

    black formatting

--- src/caching/redis_client.py
+++ src/caching/redis_client.py
@@ -3,6 +3,7 @@ from os import getenv
 import json
 from datetime import datetime
 
+
 class RedisClient:
 
     def __init__(self) -> None:
@@ -29,7 +30,7 @@ class RedisClient:
     def set_api_cache(self, url: str, payload: dict) -> bool:
         # TODO: remove hard coded expiry
         url_key = self._api_url_string(url)
-        
+
         payload.update({"cache_timestamp": datetime.now().isoformat()})
         serialized_payload = json.dumps(payload)
         self.api_cache.set(url_key, serialized_payload)
@@ -41,34 +42,21 @@ class RedisClient:
         serialized_payload = self.api_cache.get(url_key)
         if serialized_payload:
             payload: dict = json.loads(serialized_payload)
-            cache_timestamp = payload['cache_timestamp']
+            cache_timestamp = payload["cache_timestamp"]
             age = self._get_cache_age(cache_timestamp)
             payload.update({"cached": True, "cache_age": age})
-            payload.pop('cache_timestamp')
+            payload.pop("cache_timestamp")
             return payload
         return None
 
-    # def set_user_cache(self, user_id: int, payload: dict) -> bool:
-    #     id_key = self._user_id_string(user_id)
-    #     self.user_cache.hset(id_key, mapping=payload)
-    #     self.user_cache.expire(id_key, self.default_expiry)
-    #     return True
-
-    # def get_user_cache(self, user_id: int) -> dict | None:
-    #     id_key = self._user_id_string(user_id)
-    #     payload = self.user_cache.get(id_key)
-    #     if payload:
-    #         payload_dict = json.loads(payload).update({"cached": True})  # type: ignore
-    #         return payload_dict
-    #     return None
     @staticmethod
     def _get_cache_age(cache_timestamp: str) -> str:
         cache_time = datetime.fromisoformat(cache_timestamp)
         age = datetime.now() - cache_time
-        
+
         if age.days > 0:
             return f"{age.days}d"
-        
+
         minutes, seconds = divmod(age.seconds, 60)
         hours, minutes = divmod(minutes, 60)
 
